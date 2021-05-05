8f9eaccf6d3599f162d4faf07324b2e1314e6de3
Lucas Cruz <lucascs20182@gmail.com>
2021-05-05 10:18:29 -0300

    conserte navegação da tela inicial paraa cadastro de paciente

--- screens/TelaInicial.js
+++ screens/TelaInicial.js
@@ -1,15 +1,92 @@
 import React from 'react';
 import { Container, Header, Footer, Text, Tab, Tabs } from 'native-base';
 
-import List from '../screens/List';
+// import getTheme from '../native-base-theme/components';
+// import Custom from '../native-base-theme/variables/custom';
+import {
+  Content,
+  Body,
+  StyleProvider,
+  Icon,
+  Button,
+  List,
+  ListItem,
+  Title,
+  Left
+} from 'native-base';
 
-const TelaInicial = () => {
+// import store from '../redux/store';
+import {SafeAreaView, StyleSheet, ScrollView, View, Image} from 'react-native';
+import PersonRepository from '../repositories/person';
+import {useState} from 'react';
+
+const styles = StyleSheet.create({
+  
+});
+
+// import List from '../screens/List';
+
+const TelaInicial = (props) => {
+  const [people, setPeople] = useState([]);
+
+  const retrieveData = () => {
+    const repository = new PersonRepository();
+    repository.Retrieve((tx, results) => {
+      
+      let data = [];
+
+      for (let i = 0; i < results.rows.length; i++) {
+        data.push(results.rows.item(i));
+      }
+
+      // accordion?
+      // console.log(data); //array de objetos
+
+      setPeople(data);
+    });
+  };
+
+  React.useEffect(() => {
+    retrieveData();
+  }, []);
+
   return (
     <Container>
       <Header hasTabs />
       <Tabs>
         <Tab heading="Pacientes cadastrados">
-          <List />
+            <ScrollView style={styles.scrollView}>
+              <List>
+                {people.map((person, index) => (
+                  <ListItem key={`person-${index}`}>
+                    <Body>
+                      <Text>{person.name}</Text>
+                    </Body>
+                  </ListItem>
+                ))}
+              </List>
+            </ScrollView>
+
+            <View
+              style={{
+                position: 'absolute',
+                bottom: 25,
+                right: 25,
+              }}>
+              <Button
+                rounded
+                dark
+                style={{
+                  height: 50,
+                  width: 50,
+                }}
+                onPress={() => {             
+                  props.navigation.navigate('Form');
+
+                }}>
+                <Icon type="FontAwesome" name="plus" />
+              </Button>
+            </View>
         </Tab>
         <Tab heading="Médicos/Funcionários">
           <Text>conteudo2</Text>

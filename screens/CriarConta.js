2165f19725ee72ba4d1904e4c698762500815e37
RobertoSpa <robertopspagola@gmail.com>
2021-06-09 17:53:31 -0300

    username adicionado

--- screens/CriarConta.js
+++ screens/CriarConta.js
@@ -1,169 +1,167 @@
 import React, {useState, useEffect} from 'react';
 import * as Animatable from 'react-native-animatable';
 
-import {StyleSheet, Text, View, Image, 
-    TouchableWithoutFeedback, StatusBar, TextInput, 
-    SafeAreaView, Keyboard, TouchableOpacity, 
-    KeyboardAvoidingView, Animated, } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  StatusBar,
+  TextInput,
+  SafeAreaView,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Animated,
+} from 'react-native';
 
 export default function CriarConta(props) {
-    const navigation = props.navigation;
+  const navigation = props.navigation;
 
-    const [opacity] = useState(new Animated.Value(0));
-    useEffect(()=> {
-        Animated.timing(opacity, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true, //faz a maldita mensagem de erro sumir
-        }).start();
+  const [opacity] = useState(new Animated.Value(0));
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true, //faz a maldita mensagem de erro sumir
+    }).start();
+  }, []);
 
-    }, []);
+  const goToList = () => {
+    navigation.replace('CriarConta');
+  };
 
-    const goToList = () => {
-        navigation.replace('CriarConta');
-      };
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <StatusBar backgroundColor="#14207e" barStyle="light-content" />
 
-    return(
-        <KeyboardAvoidingView style={styles.background}>
-            <StatusBar backgroundColor='#14207e' barStyle='light-content'/>
+      <View style={styles.containerLogo}>
+        <Animated.Image
+          style={[
+            styles.Logo,
+            {
+              opacity: opacity,
+            },
+          ]}
+          source={require('../resources/logo.png')}
+        />
+      </View>
 
-            <View style={styles.containerLogo}>
-                <Animated.Image
-                style={[
-                    styles.Logo,
-                     {
-                        opacity: opacity,
-                     }]}
-                source={require('../resources/logo.png')}
-                />
-            </View>
+      <Animatable.View style={styles.container} animation="fadeInUpBig">
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Insira seus dados</Text>
+        </View>
 
-            <Animatable.View 
-            style={styles.container}
-            animation = "fadeInUpBig"
-            >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            autoCorrect={false}
+            onChangeText={() => {}}
+          />
 
-            <View style={styles.header}>
-                <Text style={styles.headerText}>Insira seus dados</Text>
-            </View> 
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            autoCorrect={false}
+            onChangeText={() => {}}
+          />
 
-            <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>  
-                
+          <TextInput
+            style={styles.input}
+            placeholder="Senha"
+            autoCorrect={false}
+            onChangeText={() => {}}
+          />
 
-                <TextInput
-                style={styles.input}
-                placeholder="Email"
-                autoCorrect={false}
-                onChangeText={()=> {}}
-                />
-
-                <TextInput
-                style={styles.input}
-                placeholder="Senha"
-                autoCorrect={false}
-                onChangeText={()=> {}}
-                />
-
-                <TextInput
-                style={styles.input}
-                placeholder="Repetir senha"
-                autoCorrect={false}
-                onChangeText={()=> {}}
-                />
-
-                <TouchableOpacity style={styles.btnSubmit}>
-                    <Text style={styles.submitText}>Salvar</Text>
-                </TouchableOpacity>
-
-            </View>  
-            
-
-            </Animatable.View>
-        </KeyboardAvoidingView>
-
-    );
-
+          <TouchableOpacity style={styles.btnSubmit}>
+            <Text style={styles.submitText}>Salvar</Text>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    background:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#14207e'
-    },
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#14207e',
+  },
 
-    container:{
-        backgroundColor: 'white',
-        flex: 3,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        borderTopRightRadius: 30,
-        borderTopLeftRadius: 30,
-        
-        paddingHorizontal: 30,
-        
-    },
+  container: {
+    backgroundColor: 'white',
+    flex: 3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
 
-    input:{
-        //flex: 1,
-        backgroundColor: 'white',
-        borderColor: 'grey',
-        borderWidth: 1,
-        width: 300,
-        margin: 20,
-        color: 'grey',
-        fontSize: 17,
-        borderRadius: 7,
-        padding: 10,
-        height: 50,
-        
-    },
+    paddingHorizontal: 30,
+  },
 
-    btnSubmit: {
-        //flex: 1,
-        margin: 20,
-        backgroundColor: '#117a6d',
-        width: 300,
-        height: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 7,
-    },
+  input: {
+    //flex: 1,
+    backgroundColor: 'white',
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: 300,
+    margin: 20,
+    color: 'grey',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+    height: 50,
+  },
 
-    submitText:{
-        color: 'white',
-        fontSize: 20,
+  btnSubmit: {
+    //flex: 1,
+    margin: 20,
+    backgroundColor: '#117a6d',
+    width: 300,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
 
-    },
+  submitText: {
+    color: 'white',
+    fontSize: 20,
+  },
 
+  containerLogo: {
+    flex: 1,
+    justifyContent: 'center',
+  },
 
-    containerLogo:{
-        flex: 1,
-        justifyContent: 'center',
-    },
+  Logo: {
+    width: 350,
+    height: 350,
+    resizeMode: 'contain',
+  },
 
-    Logo:{
-        width: 350,
-        height: 350,
-        resizeMode: 'contain',
-    },
+  header: {
+    //flex: 1,
+    //flexDirection: 'row',
+    justifyContent: 'center',
+    //marginBottom: 15,
+    top: 50,
+  },
 
-
-    header:{
-        //flex: 1,
-        //flexDirection: 'row',
-        justifyContent: 'center',
-        //marginBottom: 15,
-        top: 50,
-        
-        
-    },
-
-    headerText:{
-        color: '#117a6d',
-        fontWeight: 'bold',
-        fontSize: 40,
-    },
+  headerText: {
+    color: '#117a6d',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
 });
--- screens/Login.js
+++ screens/Login.js
@@ -1,209 +1,187 @@
 import React, {useState, useEffect} from 'react';
 import * as Animatable from 'react-native-animatable';
 
-import {StyleSheet, Text, View, Image, 
-    TouchableWithoutFeedback, StatusBar, TextInput, 
-    SafeAreaView, Keyboard, TouchableOpacity, 
-    KeyboardAvoidingView, Animated, } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  StatusBar,
+  TextInput,
+  SafeAreaView,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Animated,
+} from 'react-native';
 
-    import {
-        Content,
-        Body,
-        StyleProvider,
-        Icon,
-        Button,
-        List,
-        ListItem,
-        Title,
-        Left
-      } from 'native-base';
+import {
+  Content,
+  Body,
+  StyleProvider,
+  Icon,
+  Button,
+  List,
+  ListItem,
+  Title,
+  Left,
+} from 'native-base';
 
 export default function Login(props) {
-    const navigation = props.navigation;
+  const navigation = props.navigation;
 
-    const [opacity] = useState(new Animated.Value(0));
-    useEffect(()=> {
-        Animated.timing(opacity, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true, //faz a maldita mensagem de erro sumir
-        }).start();
+  const [opacity] = useState(new Animated.Value(0));
+  useEffect(() => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true, //faz a maldita mensagem de erro sumir
+    }).start();
+  }, []);
 
-    }, []);
+  const goToList = () => {
+    navigation.replace('Login');
+  };
+  return (
+    <KeyboardAvoidingView style={styles.background}>
+      <StatusBar backgroundColor="#14207e" barStyle="light-content" />
 
-    const goToList = () => {
-        navigation.replace('Login');
-      };
-    return (
-        <KeyboardAvoidingView style={styles.background}>
-            <StatusBar backgroundColor='#14207e' barStyle='light-content'/>
+      <View style={styles.containerLogo}>
+        <Animated.Image
+          style={[
+            styles.Logo,
+            {
+              opacity: opacity,
+            },
+          ]}
+          source={require('../resources/logo.png')}
+        />
+      </View>
 
-            <View style={styles.containerLogo}>
-                <Animated.Image
-                style={[
-                    styles.Logo,
-                     {
-                        opacity: opacity,
-                     }]}
-                source={require('../resources/logo.png')}
-                />
-            </View>
+      <Animatable.View style={styles.container} animation="fadeInUpBig">
+        <Animated.Image
+          style={styles.coiso}
+          source={require('../resources/coiso.png')}
+        />
 
-            <Animatable.View 
-            style={styles.container}
-            animation = "fadeInUpBig"
-            >
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
 
-            <Animated.Image
-                style={
-                    styles.coiso
-                }
-                source={require('../resources/coiso.png')}
-            
-            />    
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={() => {}}
+        />
 
-            <TextInput
-            style={styles.input}
-            placeholder="Email"
-            autoCorrect={false}
-            onChangeText={()=> {}}
-            />
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.submitText}>Acessar</Text>
+        </TouchableOpacity>
 
-            <TextInput
-            style={styles.input}
-            placeholder="Senha"
-            autoCorrect={false}
-            onChangeText={()=> {}}
-            />
-
-            <TouchableOpacity style={styles.btnSubmit}>
-                <Text style={styles.submitText}>Acessar</Text>
+        <View style={styles.viewCriarButton}>
+          <Button
+            style={styles.btnRegister}
+            onPress={() => props.navigation.navigate('CriarConta')}>
+            <TouchableOpacity>
+              <Text
+                style={styles.registerText}
+                onPress={() => props.navigation.navigate('CriarConta')}>
+                Criar conta
+              </Text>
             </TouchableOpacity>
-
-            <View style={styles.viewCriarButton}>
-                    <Button style={styles.btnRegister} onPress={() => props.navigation.navigate('CriarConta')}>
-                      <TouchableOpacity>
-                        <Text style={styles.registerText}>Criar conta</Text>
-                      </TouchableOpacity>
-                    </Button>
-            
-            </View>
-
-
-            </Animatable.View>
-        </KeyboardAvoidingView>
-    );
+          </Button>
+        </View>
+      </Animatable.View>
+    </KeyboardAvoidingView>
+  );
 }
-    
+
 const styles = StyleSheet.create({
-    background:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#14207e'
-    },
+  background: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#14207e',
+  },
 
-    container:{
-        backgroundColor: 'white',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        borderTopRightRadius: 30,
-        borderTopLeftRadius: 30,
-        
-        paddingHorizontal: 30,
-        
-    },
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
 
-    input:{
-        backgroundColor: 'white',
-        borderColor: 'grey',
-        borderWidth: 1,
-        width: '90%',
-        marginBottom: 15,
-        color: 'grey',
-        fontSize: 17,
-        borderRadius: 7,
-        padding: 10,
-        
-    },
+    paddingHorizontal: 30,
+  },
 
-    btnSubmit: {
-        marginBottom: 15,
-        backgroundColor: '#117a6d',
-        width: '90%',
-        height: 45,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 7,
-    },
+  input: {
+    backgroundColor: 'white',
+    borderColor: 'grey',
+    borderWidth: 1,
+    width: '90%',
+    marginBottom: 15,
+    color: 'grey',
+    fontSize: 17,
+    borderRadius: 7,
+    padding: 10,
+  },
 
-    submitText:{
-        color: 'white',
-        fontSize: 18,
+  btnSubmit: {
+    marginBottom: 15,
+    backgroundColor: '#117a6d',
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+  },
 
-    },
+  submitText: {
+    color: 'white',
+    fontSize: 18,
+  },
 
-    btnRegister:{
-        //margin: 14,
-        backgroundColor: 'white',
-        borderColor: '#117a6d',
-        borderWidth: 1,
-        width: '90%',
-        height: 45,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 7,
+  btnRegister: {
+    backgroundColor: 'white',
+    borderColor: '#117a6d',
+    borderWidth: 1,
+    width: '90%',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
+    resizeMode: 'contain',
+  },
 
-        //width: 70,
-        //height: 38,
-        //bottom: 25,
-        //tintColor: "#000",
-        //backgroundColor: 'rgb(232, 218, 58)',
-        resizeMode: 'contain',
-        //borderRadius: 20,
-        //justifyContent: 'center'
-        
-    },
+  registerText: {
+    color: '#117a6d',
+    fontSize: 18,
+  },
 
-    registerText:{
-        color: '#117a6d',
-        fontSize: 18,
-    },
+  containerLogo: {
+    justifyContent: 'center',
+  },
 
-    containerLogo:{
-        
-        justifyContent: 'center',
-    },
+  Logo: {
+    width: 350,
+    height: 350,
+    resizeMode: 'contain',
+  },
 
-    Logo:{
-        width: 350,
-        height: 350,
-        resizeMode: 'contain',
-    },
+  coiso: {
+    width: 80,
+    height: 80,
+    marginBottom: 25,
+  },
 
-    coiso:{
-        width: 80,
-        height: 80,
-        marginBottom: 25,
-    },
-
-    editPaciente: {
-        width: 70,
-        height: 38,
-        bottom: 25,
-        tintColor: "#000",
-        backgroundColor: 'rgb(232, 218, 58)',
-        resizeMode: 'contain',
-        borderRadius: 20,
-        justifyContent: 'center'
-    },
-
-    viewCriarButton: {
-        flexDirection: 'row'
-      },
-
+  viewCriarButton: {
+    flexDirection: 'row',
+  },
 });
-
-
